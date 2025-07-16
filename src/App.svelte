5d5c4e1d4bd2bc6157a8ae4192f0a8798fff14e8
Eero Gallano <gallanoeero@gmail.com>
2025-07-15 20:30:21 -0700

    Customized titlebar

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,11 @@
 	import HelloWorld from '$lib/components/HelloWorld.svelte';
 </script>
 
+<div data-tauri-drag-region class="w-full h-[28px] min-h-[28px] flex justify-center items-center text-sm font-bold backdrop-opacity-0 absolute top-0 left-0">
+	OpenStage
+</div>
 <main
-	class="flex min-h-screen items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4 select-none"
+	class="flex min-h-screen items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4 select-none overflow-hidden"
 >
 	<HelloWorld />
 </main>
