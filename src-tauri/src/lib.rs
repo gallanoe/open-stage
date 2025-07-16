9b8bd22ed14b969c3ada824ab2c17bf99a7bf3c1
gallanoe <42284550+gallanoe@users.noreply.github.com>
2025-07-15 20:04:46 -0700

    Initial commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,25 @@
+mod commands;
+use commands::default::{read, write};
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+/// Runs the Tauri application.
+///
+/// # Panics
+///
+/// This function will panic if the Tauri application fails to run or if there's an error during setup.
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![read, write])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
