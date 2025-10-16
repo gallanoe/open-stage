c6c62df7d27a432b8ce1bbf5da287c3ccdc41af2
Eero Gallano <gallanoeero@gmail.com>
2025-10-16 00:55:25 -0700

    Added Storybook for component development

--- svelte.config.js
+++ svelte.config.js
@@ -4,5 +4,12 @@ export default {
 	preprocess: vitePreprocess(),
 	compilerOptions: {
 		"runes": true
+	},
+	vitePlugin: {
+		dynamicCompileOptions({ filename }) {
+			if (filename.includes('node_modules')) {
+				return { runes: undefined } // or false, check what works
+			}
+		}
 	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
+/// <reference types="vitest/config" />
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname =
+	typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const host = process.env.TAURI_DEV_HOST;
-
 export default defineConfig({
-	plugins: [svelte({ compilerOptions: { runes: true } }), tailwindcss()],
+	plugins: [
+		svelte({
+			compilerOptions: {
+				runes: true
+			}
+		}),
+		tailwindcss()
+	],
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src/lib"),
-			"@/*": path.resolve(__dirname, "./src/lib/*")
+			'@': path.resolve(__dirname, './src/lib'),
+			'@/*': path.resolve(__dirname, './src/lib/*')
 		}
 	},
 	clearScreen: false,
@@ -18,7 +30,13 @@ export default defineConfig({
 		port: 1420,
 		strictPort: true,
 		host: host || false,
-		hmr: host ? { protocol: 'ws', host, port: 1421 } : undefined,
+		hmr: host
+			? {
+					protocol: 'ws',
+					host,
+					port: 1421
+				}
+			: undefined,
 		watch: {
 			ignored: ['**/src-tauri/**']
 		}
@@ -28,5 +46,33 @@ export default defineConfig({
 		target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
 		minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
 		sourcemap: !!process.env.TAURI_ENV_DEBUG
+	},
+	test: {
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					// The plugin will run tests for the stories defined in your Storybook config
+					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+					storybookTest({
+						configDir: path.join(dirname, '.storybook')
+					})
+				],
+				test: {
+					name: 'storybook',
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: 'playwright',
+						instances: [
+							{
+								browser: 'chromium'
+							}
+						]
+					},
+					setupFiles: ['.storybook/vitest.setup.ts']
+				}
+			}
+		]
 	}
 });
